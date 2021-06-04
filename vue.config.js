a3bb2c506b38de555b511dfe647528704d284226
nazocthun <nazocthun@gmail.com>
2021-06-05 05:59:11 +0800

    update title and favicon

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].title= '炉石经验计算器'
+        return args
+      })
+  }
 }
